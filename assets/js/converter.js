7aa5cad6c564a77f8255ed4d6d258e4f9574bcf0
Arthur Queiroz <arthuracq@gmail.com>
2017-05-22 23:21:19 -0300

    Added reverse conversion, UX improve on enter
    Signed-off-by: Arthur Queiroz <arthuracq@gmail.com>

--- assets/js/converter.js
+++ assets/js/converter.js
@@ -1,41 +1,38 @@
 document.getElementById("converter-button").onclick = function () {
   var link = document.getElementById("link-url").value;
-  if (link.includes("track")) {
-    var spl = link.split("/");
-    var result = "spotify:track:"+spl[4];
-    document.getElementById("result-link").value = result;
+  if (link.includes("spotify")) {
+    if (link.includes("/")) {
+      if (link.includes("track")) {
+        var spl = link.split("/");
+        var result = "spotify:track:"+spl[4];
+        document.getElementById("result-link").value = result;
+      }
+      else {
+        var spl = link.split("/");
+        var result = "spotify:user:"+spl[4]+":playlist:"+spl[6];
+        document.getElementById("result-link").value = result;
+      }
+    }
+    else {
+      if (link.includes("track")) {
+        var spl = link.split(":");
+        var result = "https://open.spotify.com/track/"+spl[2];
+        document.getElementById("result-link").value = result;
+      }
+      else {
+        var spl = link.split(":");
+        var result = "https://open.spotify.com/user/"+spl[2]+"/playlist/"+spl[4];
+        document.getElementById("result-link").value = result;
+      }
+    }
   }
   else {
-    var spl = link.split("/");
-    var result = "spotify:user:"+spl[4]+":playlist:"+spl[6];
-    document.getElementById("result-link").value = result;
+    alert("Not a Spotify link")
   }
-
-
 }
 
 document.getElementById("copy-button").onclick = function() {
   var resultlink = document.getElementById("result-link");
   resultlink.select();
-  try {
-    var successful = document.execCommand('copy');
-    // var msg = successful ? 'successful' : 'unsuccessful';
-    // console.log('Copying text command was ' + msg);
-  } catch (err) {
-    // console.log('Oops, unable to copy');
-  }
+  document.execCommand('copy');
 }
-
-// var copyTextareaBtn = document.querySelector('.js-textareacopybtn');
-//
-// copyTextareaBtn.addEventListener('click', function(event) {
-//   var copyTextarea = document.querySelector('.js-copytextarea');
-//   copyTextarea.select();
-
-// });
-
-// https://open.spotify.com/track/13sXuI5RjMHm7dz7O3spYs
-// spotify:track:13sXuI5RjMHm7dz7O3spYs
-//
-// https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX3EQWTTNVPtu
-// spotify:user:spotify:playlist:37i9dQZF1DX3EQWTTNVPtu
